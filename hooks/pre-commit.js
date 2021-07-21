22d14169c1da3cb7dc1de4ffacc608fbadaec3c5
dany <dany.d@binance.com>
2021-07-21 18:17:12 +0800

    feat: test

--- hooks/pre-commit.js
+++ hooks/pre-commit.js
@@ -1,1 +1,47 @@
-console.log('23424234234')
+const fs = require("fs");
+const path = require("path");
+const babel = require("@babel/core");
+const t = require("@babel/types");
+const shell = require("shelljs");
+const generate = require("@babel/generator").default;
+
+const getCode = () => {
+  return fs.readFileSync("index.js").toString();
+};
+
+const code = getCode();
+
+const isSchema = (node) => {
+  return (
+    node.left.property.name === "schema" && node.left.object.name === "Test"
+  );
+};
+
+let newast;
+let properties = [];
+
+const arrowFnPlugin = {
+  visitor: {
+    AssignmentExpression(path) {
+      if (isSchema(path.node)) {
+        newast = path.node.right;
+      }
+    },
+    MemberExpression(path) {
+      if (path.node.object.name === "Test") {
+        properties.push(path.node.property.name);
+      }
+    },
+  },
+};
+const r = babel.transform(code, {
+  plugins: [arrowFnPlugin],
+});
+
+const transformedCode = generate(newast).code;
+console.log("transformedCode", transformedCode);
+console.log("propertiesproperties", properties);
+
+const res = shell.exec('git show --pretty="" --name-only', (res) => {
+  console.log("eeeee", res);
+});
---
+++ index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Test = ({ py, py1, py2 }) => {};
+
+
+
+Test.actions = {};
+Test.displayName = "";
+if (process.env.julia_env !== "production") {
+  Test.schema = {
+    title: "Test",
+    type: "object",
+    properties: {
+      a: 123,
+      b: 234,
+    },
+  };
+}
+
+export default React.memo(Test);
